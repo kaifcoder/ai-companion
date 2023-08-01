1095b64c1cac69814e7ae18df5cbb30748a9e7f6
Mohd Kaif <kaifmohd2014@gmail.com>
2023-08-02 01:52:06 +0530

    categories chips created

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -2,18 +2,48 @@
 
 import { cn } from "@/lib/utils";
 import { Category } from "@prisma/client";
+import qs from "query-string";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface CategoriesProps {
   data: Category[];
 }
 
 const Categories = ({ data }: CategoriesProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const categoryId = searchParams.get("categoryId");
+  const onClick = (id: string | undefined) => {
+    const query = { categoryId: id };
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      {
+        skipNull: true,
+      }
+    );
+    router.push(url);
+  };
   return (
     <div className="w-full overflow-x-auto space-x-2 flex p-1">
+      <button
+        onClick={() => onClick(undefined)}
+        className={cn(
+          "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transistion",
+          !categoryId ? "bg-primary/25" : "bg-primary/10"
+        )}
+      >
+        Newest
+      </button>
       {data.map((category) => (
         <button
+          onClick={() => onClick(category.id)}
+          key={category.id}
           className={cn(
-            "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transistion"
+            "flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transistion",
+            category.id === categoryId ? "bg-primary/25" : "bg-primary/10"
           )}
         >
           {category.name}
