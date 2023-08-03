e9c75a1bff29d799416affe1edbe32bf7037988c
Mohd Kaif <kaifmohd2014@gmail.com>
2023-08-03 17:34:59 +0530

    updated metadata

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "res.cloudinary.com"],
+    domains: ["localhost", "res.cloudinary.com", "th.bing.com"],
   },
 };
 
