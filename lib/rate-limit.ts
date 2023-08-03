b51aad508233fc0b00e3d864c51acd3b58bc0120
Mohd Kaif <kaifmohd2014@gmail.com>
2023-08-03 18:01:59 +0530

    fix 405 try 3

--- lib/rate-limit.ts
+++ lib/rate-limit.ts
@@ -4,7 +4,7 @@ import {Redis} from "@upstash/redis";
 export async function ratelimit(identifier:string) {
     const ratelimit = new Ratelimit({
         redis: Redis.fromEnv(),
-        limiter: Ratelimit.slidingWindow(40,"50 s"),
+        limiter: Ratelimit.slidingWindow(20,"20 s"),
         analytics: true,
         prefix: "@upstash/ratelimit"
 
