dcc1cbc5a9e4f2e20e2a466d8e3b5511449c378d
Mohd Kaif <kaifmohd2014@gmail.com>
2023-08-27 23:30:20 +0530

    change db

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
   publicRoutes: [
-    "/api/webhook"
+    "/api/webhook",
+    "/"
   ]
 });
 
